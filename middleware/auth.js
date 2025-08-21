ae985208f24834c66f7445458a0629923d6f865b
Simba41 <162508682+Simba41@users.noreply.github.com>
2025-08-21 18:38:03 +0300

    fixing autorization12

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,3 @@
-const { ApiError } = require('../config/errors');
-
-function parseToken(req) 
-{
-  const h = req.headers.authorization || '';
-  return h.startsWith('Bearer ') ? h.slice(7) : null;
-}
 
 const jwt = require('jsonwebtoken');
 const { ApiError } = require('../config/errors');
