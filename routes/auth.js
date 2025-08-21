de2e0840b5161730914154b617f7f78ccb1915b5
Simba41 <162508682+Simba41@users.noreply.github.com>
2025-08-21 18:24:41 +0300

    fixing autorization12

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const { authRequired, adminRequired } = require('../middleware/auth');
-const ctrl = require('../controllers/adminController');
+const ctrl = require('../controllers/authController');
+const { authRequired } = require('../middleware/auth');
 
 const r = express.Router();
-r.use(authRequired, adminRequired);
 
-r.get('/metrics', ctrl.metrics);
-r.get('/users', ctrl.listUsers);
-r.put('/users/:id/ban', ctrl.banUser);
-r.put('/users/:id/unban', ctrl.unbanUser);
-r.get('/books', ctrl.listBooks);
-r.get('/reports', ctrl.listReports);
-r.put('/reports/:id/resolve', ctrl.resolveReport);
-r.get('/changes', ctrl.listChanges);
-r.get('/conversations', ctrl.listConversations);
-r.get('/messages', ctrl.listMessages);
+r.post('/register', ctrl.register);
+r.post('/login',    ctrl.login);
+
+r.get('/me',        authRequired, ctrl.me);
 
 module.exports = r;
