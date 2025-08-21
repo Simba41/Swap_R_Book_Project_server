715e079f89f96313bf16042658360ec44c0d45c9
Simba41 <162508682+Simba41@users.noreply.github.com>
2025-08-22 02:23:59 +0300

    fixing bugs01

--- routes/notification.js
+++ routes/notification.js
@@ -1,4 +1,3 @@
-// server/routes/notification.js
 const express = require('express');
 const { authRequired } = require('../middleware/auth');
 const ctrl = require('../controllers/notificationController');
@@ -6,7 +5,7 @@ const ctrl = require('../controllers/notificationController');
 const r = express.Router();
 
 r.get('/',               authRequired, ctrl.list);
-r.put('/read-all',       authRequired, ctrl.markAllRead);       // <-- BEFORE :id
+r.put('/read-all',       authRequired, ctrl.markAllRead);   
 r.put('/:id/read',       authRequired, ctrl.markRead);
 
 module.exports = r;
